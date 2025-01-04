9c08945ac4b4a269d83623dd73b253708251f419
Saeed Salehi <saeed.salehi1996@gmail.com>
2025-01-04 13:10:37 +0330

    [fix] console warnings

--- src/components/common/FileUploader.js
+++ src/components/common/FileUploader.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const FileUploader = ({ ...inputProps}) => {
+const FileUploader = ({ ...inputProps }) => {
   return (
-    <div class="flex items-center justify-center w-full">
+    <div className="flex items-center justify-center w-full">
       <label
-        for="dropzone-file"
-        class="flex flex-col items-center justify-center w-full lg:h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+        htmlFor="dropzone-file"
+        className="flex flex-col items-center justify-center w-full lg:h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
       >
-        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+        <div className="flex flex-col items-center justify-center pt-5 pb-6">
           <svg
-            class="w-8 h-8 mb-4 text-gray-500"
+            className="w-8 h-8 mb-4 text-gray-500"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -17,21 +17,27 @@ const FileUploader = ({ ...inputProps}) => {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
             />
           </svg>
-          <p class="mb-2 text-sm text-gray-500 text-center">
-            <span class="font-semibold">Click to upload</span> or drag and drop
+          <p className="mb-2 text-sm text-gray-500 text-center">
+            <span className="font-semibold">Click to upload</span> or drag and
+            drop
           </p>
-          <p class="text-xs text-gray-500">{inputProps.accept}</p>
+          <p className="text-xs text-gray-500">{inputProps.accept}</p>
         </div>
-        <input id="dropzone-file" type="file" class="hidden" {...inputProps} />
+        <input
+          id="dropzone-file"
+          type="file"
+          className="hidden"
+          {...inputProps}
+        />
       </label>
     </div>
   );
-}
+};
 
-export default FileUploader
+export default FileUploader;
--- src/components/common/FilterInput.js
+++ src/components/common/FilterInput.js
@@ -2,18 +2,15 @@ import React from "react";
 
 const FilterInput = ({ label, ...reset }) => {
   return (
-    <div class="relative mb-4">
-      <label
-        for="email"
-        class="block mb-2 text-sm font-medium text-gray-900"
-      >
-       {label}
+    <div className="relative mb-4">
+      <label className="block mb-2 text-sm font-medium text-gray-900">
+        {label}
       </label>
-      
+
       <input
         name="date"
         type="date"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 "
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 "
         placeholder="Select Date"
         {...reset}
       />
